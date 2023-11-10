f7f3eba9e749696817f7aa42275fe28070d29009
KASSI Diane Rolande <kassinnebie@MacBook-Pro-de-KASSI.local>
2023-11-10 15:04:02 +0100

    first commit

---
+++ js/app.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", e => {
+    //console.dir(document.children)
+
+    const version = Number(14); //type number
+    let bool = true; //type float
+    let double = 1.5; //type number
+    let speack = "Hello"; //type string
+    let type = null; //type object
+    let nombre = 10; //type indéfinie
+    let table = ["html", 1984, false];
+    let x = "10";
+    /* console.log(typeof table)
+    console.log(parseFloat(double)) */
+
+    const printout = `${speack} ${version}`; // template scripting
+    console.log(printout);
+    //condition ternaire
+
+    nombre % 2 === 0 ? console.log(`chiffre paire ${nombre}`) : console.log(`chiffre impaire ${nombre}`);
+
+    nombre === x ? console.log(true) : console.log(false);
+    console.table(table);
+    console.log(speack.split(""));
+});
